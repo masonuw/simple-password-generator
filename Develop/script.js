34717e23e0be1f6497f3c8dd7afe638317ab75fc
4jio <coxmason703@gmail.com>
2023-01-17 16:38:57 -0800

    Fixed issues.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUppercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function randomSymbol() {
+  var symbolsList = '!@#$%^&*()-_=+[]{}';
+  return symbolsList[Math.floor(Math.random() * symbolsList.length)]; 
+}
+
 function generatePassword() {
   var passwordLength = window.prompt("Please enter desired password character length (integers only).");
-  
+
   if (!isNaN(passwordLength)) () => {
   }; else return window.alert("Please try again.");
   
@@ -15,22 +32,24 @@ function generatePassword() {
   
   var symbolsOption = window.confirm("Please press OK if symbols are to be included in your password.");
 
-  for (var i = 0; i <= passwordLength; i++) {
-    if (lowercaseOption === true) {
-      randomPass += randomLowercase();
-    } else randomPass += '';
-    if (uppercaseOption === true) {
-      randomPass += randomUppercase();
-    } else randomPass += '';
-    if (numbersOption === true) {
-      randomPass += randomNumber();
-    } else randomPass += '';
-    if (symbolsOption === true) {
-      randomPass += randomSymbol();
-    } else randomPass += '';
+  var randomPass = '';
 
-    return randomPass;
+  for (var i = 0; i <= passwordLength; i++) {
+    if (lowercaseOption == true) {
+      randomPass += randomLowercase();
+    } else randomPass + '';
+    if (uppercaseOption == true) {
+      randomPass += randomUppercase();
+    } else randomPass + '';
+    if (numbersOption == true) {
+      randomPass += randomNumber();
+    } else randomPass + '';
+    if (symbolsOption == true) {
+      randomPass += randomSymbol();
+    } else randomPass + '';
   }
+
+  return randomPass;
 }
 
 // Write password to the #password input
@@ -43,23 +62,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-
-function randomLowercase() {
-  var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
-  return (Math.floor(Math.random() * lowercaseList.length));
-}
-
-function randomUppercase() {
-  var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return (Math.floor(Math.random() * uppercaseList.length));
-}
-
-function randomNumber() {
-  var numbersList = "1234567890";
-  return (Math.floor(Math.random() * numbersList.length));
-}
-
-function randomSymbol() {
-  var symbolsList = "!@#$%^&*()-_=+[]{}";
-  return (Math.floor(Math.random() * symbolsList.length)); 
-}
