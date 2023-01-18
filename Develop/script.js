382a605b10a703cc0ff1570a4ac956fdac6aeeb0
4jio <coxmason703@gmail.com>
2023-01-17 23:34:12 -0800

    Final draft.

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function randomLowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+function randomUppercase() {
+  var uppercaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return uppercaseList[Math.floor(Math.random() * uppercaseList.length)];
 }
 
-function randomUppercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function randomLowercase() {
+  var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
+  return lowercaseList[Math.floor(Math.random() * lowercaseList.length)];
 }
 
 function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  var numberList = "1234567890";
+  return numberList[Math.floor(Math.random() * numberList.length)];
 }
 
 function randomSymbol() {
-  var symbolsList = '!@#$%^&*()-_=+[]{}';
+  var symbolsList = "!@#$%^&*/-_=+";
   return symbolsList[Math.floor(Math.random() * symbolsList.length)]; 
 }
 
@@ -24,29 +27,28 @@ function generatePassword() {
   if (!isNaN(passwordLength)) () => {
   }; else return window.alert("Please try again.");
   
-  var lowercaseOption = window.confirm("Please press OK if lowercase letters are to be included in your password.");
-  
   var uppercaseOption = window.confirm("Please press OK if uppercase letters are to be included in your password.");
+
+  var lowercaseOption = window.confirm("Please press OK if lowercase letters are to be included in your password.");
   
   var numbersOption = window.confirm("Please press OK if numbers are to be included in your password.");
   
   var symbolsOption = window.confirm("Please press OK if symbols are to be included in your password.");
 
-  var randomPass = '';
-
+  var randomPass = "";
   for (var i = 0; i <= passwordLength; i++) {
-    if (lowercaseOption == true) {
-      randomPass += randomLowercase();
-    } else randomPass + '';
     if (uppercaseOption == true) {
       randomPass += randomUppercase();
-    } else randomPass + '';
+    } else randomPass + "";
+    if (lowercaseOption == true) {
+      randomPass += randomLowercase();
+    } else randomPass + "";
     if (numbersOption == true) {
       randomPass += randomNumber();
-    } else randomPass + '';
+    } else randomPass + "";
     if (symbolsOption == true) {
       randomPass += randomSymbol();
-    } else randomPass + '';
+    } else randomPass + "";
   }
 
   return randomPass;
